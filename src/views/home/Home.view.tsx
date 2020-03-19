5fc8cac9c2c41add919b9c47a173bc63933f4386
Luiz Fernando <luizepauloxd@gmail.com>
2020-03-19 02:11:30 -0300

    🔥 Removes old structure

--- src/views/home/Home.view.tsx
+++ src/views/home/Home.view.tsx
@@ -1,31 +1,12 @@
-import React, { FC, useEffect } from 'react';
-import Example from '@/components/example/Example.component';
-import api from '@/io/api';
-
-const fetchKafka = async() => {
-  console.log('LOG: fetchKafka -> fetchKafka');
-  try {
-    const { data } = await api.get('/subjects');
-    const { data: subject } = await api.get(`/subjects/${data[0]}/versions/1`);
-    console.log('LOG: fetchKafka -> subject', JSON.parse(subject.schema));
-    
-  } catch (error) {
-  console.log('LOG: fetchKafka -> error', error);
-    
-  }
-}
+import React, { FC } from 'react';
 
 const HomeView: FC = () => {
-  useEffect(() => {
-    fetchKafka();
-  }, []);
   return (
-  <>
-    Home
-    <br />
-    <Example />
-  </>
-  )
+    <>
+      Home
+      <br />
+    </>
+  );
 };
 
 export default HomeView;
