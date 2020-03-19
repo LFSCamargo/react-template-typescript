66c50326b1e1fb6c935445069676917dc7a5652d
Luiz Fernando <luizepauloxd@gmail.com>
2020-03-19 02:19:04 -0300

    🔥 Removes library

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React, { FC } from 'react';
 import { hot } from 'react-hot-loader';
-import { ThemeProvider } from 'styled-components';
-
-import { theme } from '@ben/shared-ui';
 
 import Routes from './views/Routes';
 
-const App: FC = () => (
-  <ThemeProvider theme={theme}>
-    <Routes />
-  </ThemeProvider>
-);
+const App: FC = () => <Routes />;
 
 export default hot(module)(App);
